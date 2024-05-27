339f1f9f510ca0cec9bc384d6c84a6e7da2036d9
luizgustavogithub <nogueiragustavo914@gmail.com>
2024-05-26 21:08:48 -0300

    implementção de componentes footer

---
+++ componentes/footer.js
@@ -0,0 +1,41 @@
+function criarElementoFooter() {
+    let body = document.querySelector("body");
+//Corrigir problema do link das imagens
+    body.insertAdjacentHTML('beforeend', `
+        <footer>
+            <div class="rodape">
+                <div class="logorodape">
+                    <a href="">
+                        <img src="../assets/waveblack.png" alt="">
+                    </a>
+                </div>
+
+                <div class="navega">
+                    <ul>
+                        <li><a href="#">Início</a></li>
+                        <li><a href="#">Serviços</a></li>
+                        <li><a href="#">Equipe</a></li>
+                    </ul>
+
+                    <ul>
+                        <li><a href="#">Sobre</a></li>
+                        <li><a href="#">Ajuda</a></li>
+                        <li><a href="#">Contato</a></li>
+                    </ul>
+                </div>
+
+                <div class="more">
+                    <h1>Quer saber mais sobre contabilidade e manter seu negócio ainda mais estável?<br> Assine nossa newsletter!</h1>
+                    <input placeholder="   Insira seu e-mail:" type="text">
+                    <button type="submit">INSCREVER-SE</button>
+                </div>
+            </div>
+
+            <div class="crafted"></div>
+        </footer>
+    `);
+}
+
+addEventListener("load", () => {
+    criarElementoFooter()
+});
